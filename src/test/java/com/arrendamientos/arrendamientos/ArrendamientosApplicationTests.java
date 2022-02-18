6e57ef82c4c2cf5fb877124033a2dacfb352ee36
diegoferose <diegoferose@hotmail.com>
2022-02-17 19:04:30 -0500

    configuracion incial del proyecto

---
+++ src/test/java/com/arrendamientos/arrendamientos/ArrendamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrendamientos.arrendamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrendamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
